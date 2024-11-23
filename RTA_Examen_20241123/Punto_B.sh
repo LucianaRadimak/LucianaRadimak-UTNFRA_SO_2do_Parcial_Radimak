3bac02f9be20e1789ffe08fa816a7fcb980910e3
LucianaRadimak <radimakluciana@gmail.com>
2024-11-23 05:47:51 +0000

    Añadida la carpeta RTA_Examen_20241123 correctamente al repositorio principal

---
+++ RTA_Examen_20241123/Punto_B.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/local/bin/RadimakAltaUser-Groups.sh radimak /home/radimak/repogit/UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt
+sudo cat /etc/passwd
+sudo cat /etc/shadow
+
+
+
